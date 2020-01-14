7fdb36bbc3331a94e0d1e5e147c9b67dbba22c26
John Afolabi <johnafolabi.da@gmail.com>
2020-01-14 14:13:47 +0100

    Implement get posts by id endpoint

--- data/router.js
+++ data/router.js
@@ -25,4 +25,23 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  findById(id)
+    .then(posts => {
+      if (posts.length) {
+        res.status(200).json(posts[0]);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved." });
+    });
+});
+
 module.exports = router;
