3b28a7b67bb0e06ffc43f06c0b5c6b778bed18cb
John Afolabi <johnafolabi.da@gmail.com>
2020-01-14 16:59:16 +0100

    Add cors middleware

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = require("./data/router");
+const cors = require("cors");
 
 const server = express();
 
 server.use(express.json());
+server.use(cors())
 server.use("/api/posts", router);
 
 module.exports = server;
